3e0c6cc6fea71d23a3e07d339df3a1fcd38fc30d
Aliina-ai <alinanikipelova1994@gmail.com>
2025-07-18 01:52:00 +0300

    Create users.py

---
+++ users.py
@@ -0,0 +1,12 @@
+# users.py
+
+users = {
+    "alina01": {
+        "password": "0esz257C",
+        "role": "admin"
+    },
+    "natalia01": {
+        "password": "mY2!v89rD",
+        "role": "operator"
+    }
+}
