aecfcaf1786a68d799683e31ebbf9d39e924dc56
Aliina-ai <alinanikipelova1994@gmail.com>
2025-07-27 22:15:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -784,7 +784,7 @@ def edit_region2(subscriber_id):
             request.form['first_name'],
             request.form['middle_name'],
             request.form['birth_date'],
-            request.form['street']
+            request.form['street'],
             building,
             request.form.get('apartment', ''),
             request.form['phone'],
