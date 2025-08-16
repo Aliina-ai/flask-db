dff4d5b32c61a7cbbcbe303348321407c7215c76
Aliina-ai <alinanikipelova1994@gmail.com>
2025-08-16 10:31:03 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request, url_for, redirect, session, flash, json
 import sqlite3
 import os
+import io
+from openpyxl import Workbook
+from flask import send_file
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
